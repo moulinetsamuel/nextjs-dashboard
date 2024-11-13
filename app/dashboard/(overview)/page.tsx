5e85caedbb4b7af79d745d7eafbc8c72e0243178
moulinetsamuel <samuel.moulinet@oclock.school>
2024-11-13 10:58:37 +0100

    cours fini

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,6 +8,12 @@ import {
   CardsSkeleton,
 } from "@/app/ui/skeletons";
 import CardWrapper from "@/app/ui/dashboard/cards";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description: "Your dashboard",
+};
 
 export default async function Page() {
   return (
